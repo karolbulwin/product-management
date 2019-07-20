4aa2f6cbb3253272e85ae104fc283626585a640e
Karol Bulwin <karolbulwin@gmail.com>
2019-07-20 15:34:31 +0200

    products list

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ProductService } from './product.service';
+import IProduct from './product';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+export class ProductsListComponent implements OnInit {
+  title: string = 'Product list';
+  displayedColumns: string[] = [
+    'imageUrl',
+    'productName',
+    'productCode',
+    'releaseDate',
+    'price',
+    'starRating'
+  ];
+  products: IProduct[];
+  dataSource: MatTableDataSource<IProduct>;
+  showImage: boolean = false;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+    this.dataSource = new MatTableDataSource(this.products);
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  toggleImg(): void {
+    this.showImage = !this.showImage;
+  }
+
+  onRatingClicked(message: string): void {
+    console.log(message);
+    this.title += message;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
