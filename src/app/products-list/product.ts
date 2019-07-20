3b0a2e89a01a7004f4bb0f67b20f4bec120ee600
Karol Bulwin <karolbulwin@gmail.com>
2019-07-20 15:31:02 +0200

    product interface

---
+++ src/app/products-list/product.ts
@@ -0,0 +1,10 @@
+export default interface IProduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releaseDate: string;
+  description: string;
+  price: number;
+  starRating: number;
+  imageUrl: string;
+}
