91f67edd3b9f53abe8190d8658c0267abe5eac21
Karol Bulwin <karolbulwin@gmail.com>
2019-07-21 11:13:17 +0200

    product detail

---
+++ src/app/products-list/product-detail/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import IProduct from '../product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  title: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProduct(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+}
