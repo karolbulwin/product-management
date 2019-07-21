30ec05da74b35aee87cde061008e504aaae9f2cd
Karol Bulwin <karolbulwin@gmail.com>
2019-07-21 22:03:47 +0200

    import material for proper button display-pageNotF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './home/welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductModule } from './products-list/product.module';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
-  imports: [BrowserModule, HttpClientModule, ProductModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    HttpClientModule,
+    ProductModule,
+    AppRoutingModule
+  ],
+  exports: [MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
