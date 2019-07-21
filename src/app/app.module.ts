ed019f457f30c10617d0a25329e70734d7d5940d
Karol Bulwin <karolbulwin@gmail.com>
2019-07-21 15:47:25 +0200

    refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,59 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { MatButtonModule } from '@angular/material/button';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products-list/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products-list/product-detail/product-detail.guard';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductModule } from './products-list/product.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
-    WelcomeComponent,
-    PageNotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductsListComponent },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
-      { path: 'welcome', component: WelcomeComponent },
-      { path: 'pageNotFound', component: PageNotFoundComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
-  ],
+  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
+  imports: [BrowserModule, HttpClientModule, ProductModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
