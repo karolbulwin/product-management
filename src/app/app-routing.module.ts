b49a5f6a07983b5aad06e1792620781dca08bb37
Karol Bulwin <karolbulwin@gmail.com>
2019-07-22 12:08:20 +0200

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
     path: 'welcome',
     component: WelcomeComponent,
     data: { animation: 'HomePage' }
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: { animation: 'AboutPage' }
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: { animation: 'ContactPage' }
   },
   { path: 'pageNotFound', component: PageNotFoundComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
