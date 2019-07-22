357a2ebdc94888eb8a586f268dee4e4324bcf704
Karol Bulwin <karolbulwin@gmail.com>
2019-07-22 12:08:51 +0200

    added 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ProductModule } from './products-list/product.module';
 import { MaterialModule } from './shared/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
+  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent, AboutComponent, ContactComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
