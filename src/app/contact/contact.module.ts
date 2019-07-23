69362f0d9106718cdbad7cc75b8dff8a5820b720
Karol Bulwin <karolbulwin@gmail.com>
2019-07-23 12:06:47 +0200

    contact module

---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactComponent } from './contact.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [ContactComponent, FormComponent],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
+  ]
+})
+export class ContactModule {}
